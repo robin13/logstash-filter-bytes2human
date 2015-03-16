15a63bbebcbaff17f95d87d978ad633916b44e69
Robin Clarke <robin@robinclarke.net>
2015-03-17 00:04:47 +0100

    First

---
+++ lib/logstash/filters/bytes2human.rb
@@ -0,0 +1,81 @@
+require "logstash/filters/base"
+require "logstash/filters/bytes2human/filesize"
+require "logstash/namespace"
+
+# The bytes2human filter allows conversion from text (e.g. 2.5 GB) to integer bytes
+# or from integer bytes to human readable form
+# It understands MB, MiB etc.
+
+class LogStash::Filters::Bytes2Human < LogStash::Filters::Base
+  config_name "bytes2human"
+  milestone 1
+
+  # The fields which should be converted, and the direction to convert (human, bytes)
+  # Example:
+  #
+  #     filter {
+  #       bytes2human {
+  #         # Converts 'data_size' field from 2.4 KB to 2400
+  #         convert => [ "data_size", "bytes" ]
+  #       }
+  #     }
+  #     
+  # or
+  #
+  #     filter {
+  #       bytes2human {
+  #         # Converts 'data_size' field from 123456 to "123.456 MB"
+  #         convert => [ "data_size", "human" ]
+  #       }
+  #     }
+  config :convert, :validate => :hash
+
+  public
+  def register
+    valid_directions = %w(human bytes)
+    @convert.nil? or @convert.each do |field, direction|
+      if !valid_directions.include?(direction)
+        @logger.error("Invalid conversion direction", "direction" => direction, "expected one of" => valid_directions )
+        # TODO: RCL 2014-03-19 propper 'configuration broken' exception
+        raise "Bad configuration, aborting."
+      end
+    end # @convert.each
+  end # def register
+
+  public
+  def filter(event)
+    return unless filter?(event)
+    convert(event) if @convert
+    filter_matched(event)
+  end # def filter
+
+  def convert(event)
+    @convert.each do |field, direction|
+      next unless event.include?(field)
+      original = event[field]
+      
+      if original.nil?
+        next
+      elsif original.is_a?(Hash)
+        @logger.debug("I don't know how to type convert a hash, skipping",
+                      :field => field, :value => original)
+        next
+      elsif original.is_a?(Array)
+        if direction == "human"
+          value = original.map { |v| Filesize.from(v.to_s + " B").pretty.force_encoding("UTF-8") }
+        else
+          value = original.map { |v| Filesize.from(v).to_i }
+        end
+      else
+        if direction == "human"
+          value = Filesize.from(original.to_s + " B").pretty.force_encoding("UTF-8")
+        else
+          value = Filesize.from(original).to_i;
+        end
+      end
+      event[field] = value
+    end
+  end # def convert
+
+end # class LogStash::Filters::Bytes2Human
+
---
+++ spec/filters/bytes2human_spec.rb
@@ -0,0 +1,88 @@
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/filters/bytes2human"
+
+describe LogStash::Filters::Bytes2Human do
+   
+  describe "To Bytes" do
+    # The logstash config.
+    config <<-CONFIG
+      filter {  bytes2human { convert => [ "size", "bytes" ] } }
+    CONFIG
+  
+    describe "should convert from kb" do
+      sample( "size" => "10kb" ) do
+        expect( subject["size"] ).to eq( 10000 )
+      end
+    end
+    
+    describe "should convert from mb" do
+      sample( "size" => "10MB" ) do
+        expect( subject["size"] ).to eq( 10000000 )
+      end
+    end
+    
+    describe "should convert from gb" do
+      sample( "size" => "10gb" ) do
+        expect( subject["size"] ).to eq( 10000000000 )
+      end
+    end
+
+
+    describe "should convert from tb" do
+      sample( "size" => "10.4tb" ) do
+        expect( subject["size"] ).to eq( 10400000000000 )
+      end
+    end
+
+
+    describe "should convert from mib" do
+      sample( "size" => "10.4mib" ) do
+        expect( subject["size"] ).to eq( 10905190 )
+      end
+    end
+
+    describe "should round to nearest byte - partial bytes don't exist" do
+      sample( "size" => "1.54321kb" ) do
+        expect( subject["size"] ).to eq( 1543 )
+      end
+    end
+    
+    describe "should deal undefined" do
+      sample( "size_not" => 123 ) do
+        expect( subject["size"] ).to be_nil
+      end
+    end
+  end
+  
+  describe "To Human" do
+    # The logstash config.
+    config <<-CONFIG
+      filter {  bytes2human { convert => [ "size", "human" ] } }
+    CONFIG
+  
+    describe "should convert integer to KB" do
+      sample( "size" => 123456 ) do
+        expect( subject["size"] ).to eq( "123.46 kB" )
+      end
+    end
+    
+    describe "should convert string to MB" do
+      sample( "size" => "987654321" ) do
+        expect( subject["size"] ).to eq( "987.65 MB" )
+      end
+    end
+    
+    describe "should deal with a zero value" do
+      sample( "size" => 0 ) do
+        expect( subject["size"] ).to eq( "0.00 B" )
+      end
+    end
+    
+    describe "should deal undefined" do
+      sample( "size_not" => 123 ) do
+        expect( subject["size"] ).to be_nil
+      end
+    end
+  end
+end
